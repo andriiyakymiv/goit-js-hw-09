0ee996b3a65fe0b37cf6daa2c533344944179ff6
andrii <a.iakumiv@gmail.com>
2025-03-12 14:19:34 +0200

    Import stylles for gallery

--- src/js/1-gallery.js
+++ src/js/1-gallery.js
@@ -1,15 +1,20 @@
 import SimpleLightbox from 'simplelightbox';
 
+import { images } from './1-gallery-items';
+
 const gallery = document.querySelector('.gallery');
 
 const createGallery = images.map(
   ({ preview, original, description }) => `
     <li class="gallery-item">
         <a class="gallery-link" href="${original}">
-        <img class="gallery-image" src="${preview}" data-source="${original}" alt="${description}"/>
+        <img
+            class="gallery-image"
+            src="${preview}" 
+            alt="${description}"/>
         </a>
     </li>
 `
 );
 
-gallery.insertAdjacentHTML('afterbegin', createGallery.join(''));
+gallery.innerHTML = createGallery.join('');
